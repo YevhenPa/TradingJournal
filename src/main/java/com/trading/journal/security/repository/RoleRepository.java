1953f0785adfb204ea42c283a4620ba02058885a
YevhenPa <113209010+YevhenPa@users.noreply.github.com>
2023-05-23 18:18:40 +0300

    Delete src directory

--- src/main/java/com/trading/journal/security/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trading.journal.security.repository;
-
-import com.trading.journal.security.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    Role findByName(String name);
-}
